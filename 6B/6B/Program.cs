79cfdfbf109412fb1fd4a0cd6c7c21028830c921
Mehregan Abdi <mehreganabdix@gmail.com>
2023-08-01 20:53:27 -0700

    6B Solved

---
+++ 6B/6B/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _6B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 1,1 , 0,0,4,3 };
+            Console.WriteLine(UniqueCounter.CountOfUniques(nums));
+        }
+    }
+}
---
+++ 6B/6B/UniqueCounter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6B
+{
+    public static class UniqueCounter
+    {
+        public static int CountOfUniques(int[] nums)
+        {
+            Array.Sort(nums);
+            int modalVariable = nums[0];
+            int counter = 1;
+
+            for(int i = 1; i < nums.Length; i++)
+            {
+                if(modalVariable != nums[i])
+                {
+                    counter++;
+                    modalVariable = nums[i];
+                }
+            }
+            return counter;
+        }
+    }
+}
