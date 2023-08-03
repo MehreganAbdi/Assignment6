77ef4318553e2893946462501ba3118f0e83c80d
Mehregan Abdi <mehreganabdix@gmail.com>
2023-08-02 22:01:24 -0700

    sort with bubble-sort added

--- 6B/6B/Program.cs
+++ 6B/6B/Program.cs
@@ -5,7 +5,18 @@
         static void Main(string[] args)
         {
             int[] nums = { 1, 1,1 , 0,0,4,3 };
+            int[] nums2 = { 10,4,7,13,2,4,10 };
+
+
             Console.WriteLine(UniqueCounter.CountOfUniques(nums));
+            Console.WriteLine(UniqueCounter.CountOfUniques2(nums));
+
+
+            Console.WriteLine(UniqueCounter.CountOfUniques(nums2));
+            Console.WriteLine(UniqueCounter.CountOfUniques2(nums2));
+
+
+
         }
     }
 }
--- 6B/6B/UniqueCounter.cs
+++ 6B/6B/UniqueCounter.cs
@@ -24,5 +24,56 @@ namespace _6B
             }
             return counter;
         }
+
+
+
+
+
+        public static int CountOfUniques2(int[] nums)
+        {
+            int modalVariable = nums[0];
+            int counter = 1;
+
+
+            if(nums.Length > 0)
+            {
+                for(int i = 0; i < nums.Length; i++)
+                {
+                    for (int j =0; j< nums.Length; j++)
+                    {
+                        if(modalVariable > nums[j])
+                        {
+                            Swap<int>(ref modalVariable,ref nums[j]);
+                        }
+                    }
+                }
+
+                modalVariable = nums[0];
+                foreach (var item in nums)
+                {
+                    if(modalVariable != item)
+                    {
+                        modalVariable = item;
+                        counter++;
+                    }
+                }
+            }
+            else
+            {
+                counter = 0;
+            }
+            return counter;
+        }
+
+
+
+
+        public static void Swap<T>(ref T x1 ,ref T x2)
+        {
+            T Z = x1 ;
+            x1 = x2 ;
+            x2 = Z ;
+
+        } 
     }
 }
